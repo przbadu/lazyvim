730ab5ed00e05669ef7bd706892f25f0ecb7ad6e
przbadu <pushparaj.badu@gmail.com>
2023-10-23 10:44:39 +0545

    Add onedark theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,11 +1,22 @@
 return {
+  -- catppuccino theme
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  -- one dark theme
+  {
+    "navarasu/onedark.nvim",
+    name = "onedark",
+    priority = 1000,
+    opts = {
+      style = "darker",
+    },
+  },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      -- colorscheme = "catppuccin",
+      colorscheme = "onedark",
     },
   },
 }
