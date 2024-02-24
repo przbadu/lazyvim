177b43fd93e5028be323eee5c07fa771953fd243
Pushpa Raj Badu <prz.badu@gmail.com>
2024-02-24 09:18:20 +0545

    add codi to run inline scripts

--- lua/plugins/rails.lua
+++ lua/plugins/rails.lua
@@ -4,4 +4,5 @@ return {
   { "tpope/vim-surround" },
   { "suketa/nvim-dap-ruby" }, -- debugger support using nvim-dap
   { "ngmy/vim-rubocop" }, -- rubocop for linting and formatting
+  { "metakirby5/codi.vim" }, -- print ruby code line by line
 }
